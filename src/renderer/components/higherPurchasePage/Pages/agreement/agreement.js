ddefc4e4ea0621f00b4a06ced0f80896990f6bde
Supun360 <supun@i-threesixty.com>
2019-03-08 14:44:51 +0530

    updated project with the pdf creation

--- src/renderer/components/higherPurchasePage/Pages/agreement/agreement.js
+++ src/renderer/components/higherPurchasePage/Pages/agreement/agreement.js
@@ -1,5 +1,98 @@
-import React from 'react'
-//import 
+import React from 'react';
+import { Page, Text, View,Image, Document, StyleSheet } from '@react-pdf/renderer';
+import { PDFViewer } from '@react-pdf/renderer';
+import vehicleimg from './../../../../assets/images/vehicle/vehicle.jpg'
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+    borderBottom: 1,
+    //width:'200px',
+    // height:'700px'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+    border: 1,
+  },
+  viewer:{
+    width: '100%',
+    height: '624px'
+  },
+  header:{
+    textAlign: 'center',
+    fontSize: 40
+  },
+  header2:{
+    textAlign: 'center',
+    fontSize: 20,
+    paddingBottom: 20,
+    paddingTop: 10
+  },
+  title:{
+    textAlign: 'center',
+    fontSize: 25,
+    borderBottom: 1,
+    
+  },
+  image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
+  },
+
+});
+
+
+var data = {
+  companyName: 'I360 Pvt Ltd',
+  Title: 'Vehicle Higher Purchasing Agreement',
+  user:{
+    Name: 'A.D.S.Nimesh',
+    Address: '533/1, Batagama South, Kandana',
+    Telephone: '0774560583',
+  },
+  vehicle:{
+
+  },
+  paymentDetails:{
+
+  },
+}
+
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text style={styles.header}>{data.companyName}</Text>
+        <Text style={styles.title}>{data.Title}</Text>
+       
+       
+        <Text style={styles.header2}>Customer Details</Text>
+        <Text>NIC: </Text>
+        <Text>Name: </Text>
+        <Text>Address: </Text>
+        <Text>Tel: </Text>
+        <Text style={styles.header2}>Vehicle Deatils</Text>
+        <Text>Vehicle No: </Text>
+        <Text>Vehicle Chassi No: </Text>
+        <Text>Engine No: </Text>
+        <Text>Vehicle Model: </Text>
+        <Text>Vehicle Made Year: </Text>
+        <Text>Vehicle Color: </Text>
+        <Text style={styles.header2}>Payment Details</Text>
+        <Text>Monthly Rental:</Text>
+        <Text>Due dates:</Text>
+        <Text>Interest:</Text>
+       {/* /</View>/ <></ */}
+      </View>
+    </Page>
+  </Document>
+);
+
 class agreement extends React.Component {
     render() {
       return (
@@ -22,7 +115,9 @@ class agreement extends React.Component {
     {/*END TITLE & BREADCRUMB PAGE*/}
     {/*BEGIN CONTENT*/}
     <div className="page-content">
-      
+    <PDFViewer style={styles.viewer}>
+      <MyDocument/>
+    </PDFViewer>
     </div>
     {/*END CONTENT*/}
     {/*END FOOTER*/}
